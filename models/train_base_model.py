f49d5d41acd5b17d0d2fdeebc9d764ca03604ced
Ashwin Devaraj <ashwin.dev.pro@gmail.com>
2021-11-29 11:06:41 -0600

    cleaned some eval code

--- models/train_base_model.py
+++ models/train_base_model.py
@@ -5,6 +5,7 @@ from transformers import RobertaForSequenceClassification, BartForSequenceClassi
 from os import mkdir, system
 from os.path import isdir
 from tqdm import tqdm
+from utils import evaluate
 
 def get_dirname(args):
     return f'trained_models/{args.model}_base_epochs-{args.num_train_epochs}_bs-{args.batch_size}_lr-{args.learning_rate}'
@@ -76,44 +77,6 @@ def get_predictions(model, dataset, batch_size):
 
     preds = torch.cat(preds)
     return preds
-
-def tensor_accuracy(preds, targets):
-    return torch.sum(preds.eq(targets).int()).item()/len(preds)
-
-def tensor_binary_precision(preds, targets):
-    num_labeled_positive = torch.sum(preds).item()
-    if num_labeled_positive == 0:
-        return 0
-    num_correctly_labeled_positive = torch.sum(preds * preds.eq(targets).int()).item()
-    return num_correctly_labeled_positive/num_labeled_positive 
-
-def tensor_binary_recall(preds, targets):
-    num_truly_positive = torch.sum(targets).item()
-    if num_truly_positive==0:
-        return 0
-    num_correctly_labeled_positive = torch.sum(preds * preds.eq(targets).int()).item()
-    return num_correctly_labeled_positive/num_truly_positive
-
-def tensor_binary_f1(preds, targets):
-    precision = tensor_binary_precision(preds, targets)
-    recall = tensor_binary_recall(preds, targets)
-    if precision + recall == 0:
-        return 0
-    return 2 * precision * recall / (precision + recall)
-
-def multiclass_f1(preds, targets):
-    scores = []
-    for i in range(3):
-        scores.append(tensor_binary_f1(preds.eq(i).int(), targets.eq(i).int()))
-    scores.append(sum(scores)/3)
-    return scores
-
-def evaluate(model, dataset, batch_size):
-    preds = get_predictions(model, dataset, batch_size)
-    targets = torch.LongTensor(dataset.y_label)
-    n_f1, e_f1, c_f1, m_f1 =  multiclass_f1(preds, targets)
-    acc = tensor_accuracy(preds, targets)
-    return n_f1, e_f1, c_f1, m_f1, acc
 
 def main(args):
     if args.task=='train':
---
+++ models/utils.py
@@ -0,0 +1,62 @@
+import torch
+
+def get_iter_indices(batch_size, length):
+    indices = []
+    i = 0
+    while i < length:
+        indices.append(i)
+        i = min(i+batch_size, length)
+    return indices
+
+@torch.no_grad()
+def get_predictions(model, dataset, batch_size):
+    model.eval()
+    preds = []
+    indices = get_iter_indices(batch_size, len(dataset))
+    for i in indices:
+        j = min(i+batch_size, len(dataset))
+        batch = dataset[i:j]
+        del batch['labels']
+        logits = model(**batch)[0]
+        preds.append(torch.argmax(logits, dim=-1).to('cpu'))
+
+    preds = torch.cat(preds)
+    return preds
+
+def tensor_accuracy(preds, targets):
+    return torch.sum(preds.eq(targets).int()).item()/len(preds)
+
+def tensor_binary_precision(preds, targets):
+    num_labeled_positive = torch.sum(preds).item()
+    if num_labeled_positive == 0:
+        return 0
+    num_correctly_labeled_positive = torch.sum(preds * preds.eq(targets).int()).item()
+    return num_correctly_labeled_positive/num_labeled_positive 
+
+def tensor_binary_recall(preds, targets):
+    num_truly_positive = torch.sum(targets).item()
+    if num_truly_positive==0:
+        return 0
+    num_correctly_labeled_positive = torch.sum(preds * preds.eq(targets).int()).item()
+    return num_correctly_labeled_positive/num_truly_positive
+
+def tensor_binary_f1(preds, targets):
+    precision = tensor_binary_precision(preds, targets)
+    recall = tensor_binary_recall(preds, targets)
+    if precision + recall == 0:
+        return 0
+    return 2 * precision * recall / (precision + recall)
+
+def multiclass_f1(preds, targets):
+    scores = []
+    for i in range(3):
+        scores.append(tensor_binary_f1(preds.eq(i).int(), targets.eq(i).int()))
+    scores.append(sum(scores)/3)
+    return scores
+
+def evaluate(model, dataset, batch_size):
+    preds = get_predictions(model, dataset, batch_size)
+    targets = torch.LongTensor(dataset.y_label)
+    n_f1, e_f1, c_f1, m_f1 =  multiclass_f1(preds, targets)
+    acc = tensor_accuracy(preds, targets)
+    return n_f1, e_f1, c_f1, m_f1, acc
